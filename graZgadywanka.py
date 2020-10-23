4e61c99306a365d5b42e2288e7c4bce722dfbcf5
ferbjava <tooper@wp.pl>
2020-10-23 18:08:27 +0200

    for loop and list

--- graZgadywanka.py
+++ graZgadywanka.py
@@ -5,9 +5,11 @@ print("Witaj w grze 'zgadywanka'!")
 gornyLimit = int(input("W jakim zakresie mam wygenerować liczbę do zgadnięcia? "))
 wynikLosowania = random.randint(1, gornyLimit)
 musiZgadywac = True
+listaRuchow = list()
 
 while musiZgadywac:
     typUzytkownika = int(input("Podaj liczbe: "))
+    listaRuchow.append(typUzytkownika)
     if wynikLosowania == typUzytkownika:
         musiZgadywac = False
         print("Brawo, zgadłeś!")
@@ -15,4 +17,10 @@ while musiZgadywac:
         print("Niestety, nie jest to prawidłowa odpowiedź :-( Spróbuj jeszcze raz!")
 
 print("koniec gry!")
+
+decyzja = input("Czy wyświetlić listę twóich ruchów? [T/N]")
+if decyzja.upper() == "T":
+    for ruch in listaRuchow:
+        print("kolejny ruch: ", ruch)
+
 input()
---
+++ loopExample.py
@@ -0,0 +1,9 @@
+liczba = int(input("Podaj liczbe: "))
+mojaLista = list()
+mojaLista.append(liczba)
+mojaLista.append(4)
+print(mojaLista)
+
+for liczba in mojaLista:
+    print(liczba)
+input()
