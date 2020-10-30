5e59ad884149bf3fa63c1a90fa179c2f2fff3bfb
ferbjava <tooper@wp.pl>
2020-10-30 18:00:03 +0100

    tdd tasks

---
+++ tdd/npower.py
@@ -0,0 +1,6 @@
+def index(array, n):
+    return 0
+
+
+assert index([1, 2, 3, 4],2) == 9
+assert index([1, 3, 10, 100],3) == 1000000
---
+++ tdd/squareSum.py
@@ -0,0 +1,6 @@
+def square_sum(numbers):
+    return 0
+
+
+assert square_sum([1, 2]) == 5
+assert square_sum([0, 3, 4, 5]) == 50
