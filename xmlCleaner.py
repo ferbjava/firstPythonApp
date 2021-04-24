429f74413a4e58086c04f442c357d2e6c867638b
ferbjava <tooper@wp.pl>
2021-04-24 21:11:08 +0200

    xmlCleaner ready

--- xmlCleaner.py
+++ xmlCleaner.py
@@ -1,5 +1,23 @@
 print("Program will remove unneeded lines in bpmn2 files")
 filename = "bpmn2.xml"
+
+def checkLabels(line):
+    global writeLine, lines2skip
+    label = line.find("BPMNLabel")
+    if label != -1:
+        l = len(line)
+        endingSlash = line.find("/")
+        if endingSlash == -1:
+            line2 = lines[index + 2]
+            sndEndingSlash = line2.strip().find("/")
+            endingLabel = line2.find("BPMNLabel")
+            if sndEndingSlash == 1 and endingLabel != -1:
+                writeLine = False
+                lines2skip = 2
+        elif (l - endingSlash) == 3:
+            writeLine = False
+
+
 with open(filename, "r") as f:
     lines = f.readlines()
 with open(filename, "w") as f:
@@ -7,14 +25,14 @@ with open(filename, "w") as f:
     for index, line in enumerate(lines):
         writeLine = True
         if lines2skip > 0:
+            print("line ", index + 1, " removed:", line)
+            lines2skip -= 1
             continue
-        label = line.find("BPMNLabel")
-        if label != -1:
-            l = len(line)
-            endingSlash = line.find("/")
-            if endingSlash == -1:
-                writeLine = True
-            elif (l - endingSlash) == 3:
-                writeLine = False
+
+        checkLabels(line)
+
         if writeLine:
             f.write(line)
+        else:
+            print("line ", index + 1, " removed:", line)
+
